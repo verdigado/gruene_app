9027be82ce512b4e900799937343824eda7eef5f
Lars Boß <lars.boss@prodyna.com>
2023-02-24 08:59:18 +0100

    feat: extended login screen
    Added slider carousel and more (see in design)

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,19 +1,162 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:gruene_app/gen/assets.gen.dart';
+import 'package:url_launcher/link.dart';
+import '../../widget/slider_carousel.dart';
+import '../../routing/routes.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          Expanded(child: SvgPicture.asset(Assets.images.gRUENETopicOekologie)),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                child: SliderCarousel(
+              showPrevNextButton: true,
+              showIndicator: true,
+              backgroundColor: Colors.white,
+              activeDotColor: Theme.of(context).colorScheme.primary,
+              deactiveDotColor: const Color(0xFFD9D9D9),
+              iconColor: Colors.white,
+              rightIcon: Icons.arrow_forward,
+              leftIcon: Icons.arrow_back,
+              iconSize: 35,
+              backgroundImage: SvgPicture.asset(
+                  Assets.images.gRUENETopicOekologie,
+                  height: size.height / 100 * 60),
+              controlsBackground: Theme.of(context).primaryColor,
+              pages: [
+                SliderCarouselPage(
+                  backgroundColorText: Theme.of(context).primaryColor,
+                  backgroundColorImage: Colors.blue,
+                  preserveImageSpace: false,
+                  title: "Business Chat",
+                  body:
+                      "First impressions are everything in business, even in chat service. It’s important to show professionalism and courtesy from the start",
+                ),
+                SliderCarouselPage(
+                  backgroundColorText: Theme.of(context).primaryColor,
+                  preserveImageSpace: false,
+                  title: "Coffee With Friends",
+                  body:
+                      "When your morning starts with a cup of coffee and you are used to survive the day with the same, then all your Instagram stories and snapchat streaks would stay filled up with coffee pictures",
+                ),
+                SliderCarouselPage(
+                  backgroundColorText: Theme.of(context).primaryColor,
+                  preserveImageSpace: false,
+                  title: "Mobile Application",
+                  body:
+                      "Mobile content marketing has also been found to enhance quick online actions and make follow-ups easier.",
+                ),
+                SliderCarouselPage(
+                  backgroundColorText: Theme.of(context).primaryColor,
+                  preserveImageSpace: false,
+                  title: "Content Team",
+                  body: "No two content marketing teams look the same.",
+                ),
+              ],
+            )),
+            ElevatedButton(
+              onPressed: () => context.go(login),
+              child: Text('Anmelden',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge
+                      ?.copyWith(color: Colors.white)),
+            ),
+            const SizedBox(height: 30),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: const [
+              SizedBox(width: 10),
+              Text('Noch kein Mitglied?'),
+              SizedBox(width: 10),
+              Text(
+                'Jetzt Mitglied werden',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ]),
+            const SizedBox(height: 20),
+            const DatImpContainer(),
+            const SizedBox(height: 10),
+          ],
+        ),
       ),
-    ));
+    );
+  }
+}
+
+class AppLocalizations {
+  static of(BuildContext context) {}
+}
+
+class DatImpContainer extends StatelessWidget {
+  const DatImpContainer({super.key});
+// TODO: Make Links(2) work
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Link(
+                  target: LinkTarget.blank,
+                  uri: Uri(
+                      scheme: 'https',
+                      host: 'www.gruene.de',
+                      path: '/service/datenschutz'),
+                  builder: (context, followLink) {
+                    return GestureDetector(
+                      onTap: followLink,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Datenschutz',
+                              style: Theme.of(context).textTheme.bodySmall),
+                        ],
+                      ),
+                    );
+                  }),
+              const SizedBox(width: 10),
+              Container(
+                width: 5,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Link(
+                  target: LinkTarget.blank,
+                  uri: Uri(
+                      scheme: 'https',
+                      host: 'www.gruene.de',
+                      path: '/service/impressum'),
+                  builder: (context, followLink) {
+                    return GestureDetector(
+                      onTap: followLink,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Impressum',
+                              style: Theme.of(context).textTheme.bodySmall),
+                        ],
+                      ),
+                    );
+                  }),
+            ],
+          )),
+    );
   }
 }
