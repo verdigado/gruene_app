21f62b17ae598e5b19136662c53d2223d51bfa47
DominikP30 <dominik.peters@prodyna.com>
2023-02-27 17:49:35 +0100

    fix naming

--- lib/widget/privacy_imprint.dart
+++ lib/widget/privacy_imprint.dart
@@ -5,8 +5,8 @@ import 'package:url_launcher/url_launcher.dart';
 class DatImpContainer extends StatelessWidget {
   DatImpContainer({super.key});
 
-  final Uri _url_imprint = Uri.parse('https://www.gruene.de/service/impressum');
-  final Uri _url_privacy =
+  final Uri _urlImprint = Uri.parse('https://www.gruene.de/service/impressum');
+  final Uri _urlPrivacy =
       Uri.parse('https://www.gruene.de/service/datenschutz');
 
   Future<void> _launchUrl(Uri url) async {
@@ -25,7 +25,7 @@ class DatImpContainer extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           TextButton(
-            onPressed: () => _launchUrl(_url_privacy),
+            onPressed: () => _launchUrl(_urlPrivacy),
             child: Text(AppLocalizations.of(context)!.privacyPolicy,
                 style: Theme.of(context).primaryTextTheme.bodySmall),
           ),
@@ -40,7 +40,7 @@ class DatImpContainer extends StatelessWidget {
           ),
           const SizedBox(width: 10),
           TextButton(
-            onPressed: () => _launchUrl(_url_imprint),
+            onPressed: () => _launchUrl(_urlImprint),
             child: Text(AppLocalizations.of(context)!.imprint,
                 style: Theme.of(context).primaryTextTheme.bodySmall),
           ),
