993696f0eeca109fc5e02cc8ea92ed8ce45999f1
DominikP30 <dominik.peters@prodyna.com>
2023-03-03 17:46:55 +0100

    #42 fixTests

--- test/screens/onboarding/onboarding_screen_test.dart
+++ test/screens/onboarding/onboarding_screen_test.dart
@@ -12,141 +12,164 @@ import 'package:gruene_app/screens/onboarding/onboarding_layout.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mocktail/mocktail.dart';
 
-Set<Topic> topics = {
-  Topic(
-      id: "1",
-      name: "Testen macht Spaß",
-      imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
-  Topic(
-      id: "2",
-      name: "Testen",
-      imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
-  Topic(
-      id: "3",
-      name: "Flutter",
-      imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
-  Topic(
-      id: "4",
-      name: "Flutter",
-      imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
-  Topic(
-      id: "5",
-      name: "Flutter",
-      imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
-  Topic(
-      id: "6",
-      name: "Flutter",
-      imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
-  Topic(
-      id: "7",
-      name: "Flutter",
-      imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png')
-};
-Set<Subject> subjects = {
-  Subject(id: '1', name: 'Test1'),
-  Subject(id: '2', name: 'Test2'),
-  Subject(id: '3', name: 'Test3')
-};
-
 void main() {
-  setUp(() {
-    GetIt.instance.registerSingleton(
-      instanceName: 'TopicCard',
-      ImageProviderDelegate(typ: ImageProviderTyp.asset),
-    );
-  });
-  testWidgets(
-      'should_send_all_interessets_And_Subjects_when_check_all_interessets_And_Subjects',
-      (tester) async {
-    MockOnboardingRepository onboardingRepositoryMock =
-        MockOnboardingRepository();
-    when(() => onboardingRepositoryMock.listTopic()).thenReturn(topics);
-    when(() => onboardingRepositoryMock.listSubject()).thenReturn(subjects);
-    when(() => onboardingRepositoryMock.onboardingSend(any(), any()))
-        .thenReturn(true);
-    final bloc = OnboardingBloc(onboardingRepositoryMock);
+  group('Onboarding', () {
+    setUpAll(() async {
+      GetIt.instance.registerSingleton(
+        instanceName: 'TopicCard',
+        ImageProviderDelegate(typ: ImageProviderTyp.asset),
+      );
+    });
+    tearDown(() async {
+      resetMocktailState();
+    });
+    testWidgets(
+        'should_send_all_interessets_And_Subjects_when_check_all_interessets_And_Subjects',
+        (tester) async {
+      Set<Topic> topics = {
+        Topic(
+            id: "1",
+            name: "Testen macht Spaß",
+            imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
+        Topic(
+            id: "2",
+            name: "Testen",
+            imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
+        Topic(
+            id: "3",
+            name: "Flutter",
+            imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
+        Topic(
+            id: "4",
+            name: "Flutter",
+            imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
+        Topic(
+            id: "5",
+            name: "Flutter",
+            imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
+        Topic(
+            id: "6",
+            name: "Flutter",
+            imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
+        Topic(
+            id: "7",
+            name: "Flutter",
+            imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png')
+      };
+      Set<Subject> subjects = {
+        Subject(id: '1', name: 'Test1'),
+        Subject(id: '2', name: 'Test2'),
+        Subject(id: '3', name: 'Test3')
+      };
+      MockOnboardingRepository onboardingRepositoryMock =
+          MockOnboardingRepository();
+      when(() => onboardingRepositoryMock.listTopic()).thenReturn(topics);
+      when(() => onboardingRepositoryMock.listSubject()).thenReturn(subjects);
+      when(() => onboardingRepositoryMock.onboardingSend(any(), any()))
+          .thenReturn(true);
+      final bloc = OnboardingBloc(onboardingRepositoryMock);
 
-    await tester.pumpWidget(makeTestWidget(const OnboardingLayout(), bloc));
-    bloc.add(OnboardingLoad());
-    await tester.pumpAndSettle();
+      await tester.pumpWidget(makeTestWidget(const OnboardingLayout(), bloc));
+      bloc.add(OnboardingLoad());
+      await tester.pumpAndSettle();
 
-    await tester.tap(find.byKey(const Key('intro_page_next_step')));
-    // Because of the PageTransition Animation we need to wait for 1 seconds
-    await tester.pumpAndSettle(const Duration(seconds: 1));
-    for (var topic in topics) {
-      await tester.tap(find.byKey(Key('TopicCard_${topic.id}')));
+      await tester.tap(find.byKey(const Key('intro_page_next_step')));
+      // Because of the PageTransition Animation we need to wait for 1 seconds
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      for (var topic in topics) {
+        await tester.tap(find.byKey(Key('TopicCard_${topic.id}')));
+        await tester.pump();
+        // The grid is 2 x 2 this is the reason that we scroll on every second Card
+        if (int.parse(topic.id) % 2 == 0) {
+          await tester.drag(find.byKey(const Key('Onboarding_PageView')),
+              const Offset(0.0, -400));
+          await tester.pump();
+        }
+      }
+      await tester.tap(find.byKey(const Key('interests_page_next_step')));
+      await tester.pumpAndSettle(const Duration(seconds: 2));
+      for (var sub in subjects) {
+        await tester.tap(find.widgetWithText(ListTile, sub.name));
+      }
+      await tester.pump();
+      bloc.add(OnboardingDone());
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      verify(
+        () => onboardingRepositoryMock.onboardingSend(
+          any(that: containsAll(topics)),
+          any(that: containsAll(subjects)),
+        ),
+      );
+      verify(
+        () => onboardingRepositoryMock.listTopic(),
+      ).called(1);
+      verify(
+        () => onboardingRepositoryMock.listSubject(),
+      ).called(1);
+    });
+
+    testWidgets(
+        'should_send_first_interessets_And__first_Subjects_when_check_first_interessets_And_Subjects',
+        (tester) async {
+      Set<Topic> topics = {
+        Topic(
+            id: "1",
+            name: "Testen macht Spaß",
+            imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
+        Topic(
+            id: "2",
+            name: "Testen",
+            imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
+        Topic(
+            id: "3",
+            name: "Flutter",
+            imageUrl: 'assets/images/Sonnenblume_rgb_aufTransparent.png'),
+      };
+      Set<Subject> subjects = {
+        Subject(id: '1', name: 'Test1'),
+        Subject(id: '2', name: 'Test2'),
+        Subject(id: '3', name: 'Test3')
+      };
+      MockOnboardingRepository onboardingRepositoryMock =
+          MockOnboardingRepository();
+      when(() => onboardingRepositoryMock.listTopic()).thenReturn(topics);
+      when(() => onboardingRepositoryMock.listSubject()).thenReturn(subjects);
+      when(() => onboardingRepositoryMock.onboardingSend(any(), any()))
+          .thenReturn(true);
+      final bloc = OnboardingBloc(onboardingRepositoryMock);
+
+      await tester.pumpWidget(makeTestWidget(const OnboardingLayout(), bloc));
+      bloc.add(OnboardingLoad());
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(const Key('intro_page_next_step')));
+      // Because of the PageTransition Animation we need to wait for 1 seconds
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+
+      await tester.tap(find.byKey(Key('TopicCard_${topics.first.id}')));
       await tester.pump();
       // The grid is 2 x 2 this is the reason that we scroll on every second Card
-      if (int.parse(topic.id) % 2 == 0) {
-        await tester.drag(find.byKey(const Key('Onboarding_PageView')),
-            const Offset(0.0, -400));
-        await tester.pump();
-      }
-    }
-    await tester.tap(find.byKey(const Key('interests_page_next_step')));
-    await tester.pumpAndSettle(const Duration(seconds: 2));
-    for (var sub in subjects) {
-      await tester.tap(find.widgetWithText(ListTile, sub.name));
-    }
-    await tester.pump();
-    bloc.add(OnboardingDone());
-    await tester.pumpAndSettle(const Duration(seconds: 1));
-    verify(
-      () => onboardingRepositoryMock.onboardingSend(
-        any(that: containsAll(topics)),
-        any(that: containsAll(subjects)),
-      ),
-    );
-    verify(
-      () => onboardingRepositoryMock.listTopic(),
-    ).called(1);
-    verify(
-      () => onboardingRepositoryMock.listSubject(),
-    ).called(1);
-  });
+      await tester.tap(find.byKey(const Key('interests_page_next_step')));
+      await tester.pumpAndSettle(const Duration(seconds: 2));
 
-  testWidgets(
-      'should_send_first_interessets_And__first_Subjects_when_check_first_interessets_And_Subjects',
-      (tester) async {
-    MockOnboardingRepository onboardingRepositoryMock =
-        MockOnboardingRepository();
-    when(() => onboardingRepositoryMock.listTopic()).thenReturn(topics);
-    when(() => onboardingRepositoryMock.listSubject()).thenReturn(subjects);
-    when(() => onboardingRepositoryMock.onboardingSend(any(), any()))
-        .thenReturn(true);
-    final bloc = OnboardingBloc(onboardingRepositoryMock);
+      await tester.tap(find.widgetWithText(ListTile, subjects.first.name));
 
-    await tester.pumpWidget(makeTestWidget(const OnboardingLayout(), bloc));
-    bloc.add(OnboardingLoad());
-    await tester.pumpAndSettle();
-
-    await tester.tap(find.byKey(const Key('intro_page_next_step')));
-    // Because of the PageTransition Animation we need to wait for 1 seconds
-    await tester.pumpAndSettle(const Duration(seconds: 1));
-
-    await tester.tap(find.byKey(Key('TopicCard_${topics.first.id}')));
-    await tester.pump();
-    // The grid is 2 x 2 this is the reason that we scroll on every second Card
-    await tester.tap(find.byKey(const Key('interests_page_next_step')));
-    await tester.pumpAndSettle(const Duration(seconds: 2));
-
-    await tester.tap(find.widgetWithText(ListTile, subjects.first.name));
-
-    await tester.pump();
-    bloc.add(OnboardingDone());
-    await tester.pumpAndSettle(const Duration(seconds: 1));
-    verify(
-      () => onboardingRepositoryMock.onboardingSend(
-        any(that: containsAllInOrder([topics.first])),
-        any(that: containsAllInOrder([subjects.first])),
-      ),
-    );
-    verify(
-      () => onboardingRepositoryMock.listTopic(),
-    ).called(1);
-    verify(
-      () => onboardingRepositoryMock.listSubject(),
-    ).called(1);
+      await tester.pump();
+      bloc.add(OnboardingDone());
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      verify(
+        () => onboardingRepositoryMock.onboardingSend(
+          any(that: containsAllInOrder([topics.first])),
+          any(that: containsAllInOrder([subjects.first])),
+        ),
+      );
+      verify(
+        () => onboardingRepositoryMock.listTopic(),
+      ).called(1);
+      verify(
+        () => onboardingRepositoryMock.listSubject(),
+      ).called(1);
+    });
   });
 }
 
