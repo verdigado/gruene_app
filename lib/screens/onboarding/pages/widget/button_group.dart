f165d1078444fc5c564d0488856ccd9dc5ff62eb
Lars Boß <lars.boss@prodyna.com>
2023-03-09 10:51:58 +0100

    Converted button-group in onboarding to separate widget

---
+++ lib/screens/onboarding/pages/widget/button_group.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../../../../constants/theme_data.dart';
+
+class ButtonGroupNextPrevious extends StatefulWidget {
+  final VoidCallback? next;
+  final String nextText;
+  final VoidCallback? previous;
+  final String previousText;
+
+  const ButtonGroupNextPrevious({
+    Key? key,
+    this.next,
+    this.nextText = 'Next',
+    this.previous,
+    this.previousText = 'Previous',
+  }) : super(key: key);
+
+  @override
+  State<ButtonGroupNextPrevious> createState() =>
+      _ButtonGroupNextPreviousState();
+}
+
+class _ButtonGroupNextPreviousState extends State<ButtonGroupNextPrevious> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 1),
+        Container(
+          height: 10,
+          decoration: const BoxDecoration(
+            border: Border(
+              top: BorderSide(
+                  width: 1, color: lightGrey, style: BorderStyle.solid),
+            ),
+          ),
+        ),
+        ElevatedButton(
+            onPressed: widget.next,
+            child: Text(widget.nextText,
+                style: textTheme.labelLarge?.copyWith(color: Colors.white))),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: widget.previous,
+          style: grayButtonStyle,
+          child: Text(widget.previousText,
+              style: textTheme.labelLarge?.copyWith(color: darkGrey)),
+        ),
+        const SizedBox(height: 20),
+      ],
+    );
+  }
+}
