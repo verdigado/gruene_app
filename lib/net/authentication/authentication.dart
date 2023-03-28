10153f1c8373fbd81c660306311d157d4cfd30b1
DominikP30 <dominik.peters@prodyna.com>
2023-03-28 17:35:14 +0200

    add auth Logic

--- lib/net/authentication/authentication.dart
+++ lib/net/authentication/authentication.dart
@@ -1,30 +1,54 @@
 import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:gruene_app/common/logger.dart';
 
-void refreshToken(String? refreshToken) async {
+enum SecureStoreKeys {
+  accesToken,
+  refreshtoken,
+  accessTokenExpiration,
+  refreshExpiresIn
+}
+
+const authStorage = FlutterSecureStorage(
+  aOptions: AndroidOptions(
+    encryptedSharedPreferences: true,
+  ),
+);
+
+Future<bool> refreshToken(String? refreshToken) async {
   const appAuth = FlutterAppAuth();
-  var res = await appAuth.token(TokenRequest(
-    'gruene_app',
-    'grueneapp://appAuth?prompt=login',
-    discoveryUrl:
-        'https://saml.gruene.de/realms/gruenes-netz/.well-known/openid-configuration',
-    refreshToken: refreshToken,
-    scopes: [
-      "openid",
-      "address",
-      "acr",
-      "email",
-      "web-origins",
-      "oauth-einverstaendniserklaerung",
-      "oauth-username",
-      "roles",
-      "profile",
-      "phone",
-      "offline_access",
-      "microprofile-jwt"
-    ],
-  ));
-  res = res;
+  try {
+    var res = await appAuth.token(
+      TokenRequest(
+        'gruene_app',
+        'grueneapp://appAuth?prompt=login',
+        discoveryUrl:
+            'https://saml.gruene.de/realms/gruenes-netz/.well-known/openid-configuration',
+        refreshToken: refreshToken,
+        scopes: [
+          "openid",
+          "address",
+          "acr",
+          "email",
+          "web-origins",
+          "oauth-einverstaendniserklaerung",
+          "oauth-username",
+          "roles",
+          "profile",
+          "phone",
+          "offline_access",
+          "microprofile-jwt"
+        ],
+      ),
+    );
+    if (res == null) {
+      return false;
+    }
+    saveTokensInSecureStorage(res);
+  } on Exception catch (e, st) {
+    logger.d('Fail on Authentication', [e, st]);
+    return false;
+  }
 }
 
 Future<bool> startLogin() async {
@@ -52,10 +76,54 @@ Future<bool> startLogin() async {
         ],
       ),
     );
+    if (result == null) {
+      return false;
+    }
+    saveTokensInSecureStorage(result);
   } on Exception catch (e, st) {
     logger.d('Fail on Authentication', [e, st]);
     return false;
   }
-  // Save Token
+  return true;
+}
+
+void saveTokensInSecureStorage(AuthorizationTokenResponse tokenResponse) {
+  authStorage.write(
+      key: SecureStoreKeys.accesToken.name, value: tokenResponse.accessToken);
+  authStorage.write(
+      key: SecureStoreKeys.refreshtoken.name,
+      value: tokenResponse.refreshToken);
+  authStorage.write(
+      key: SecureStoreKeys.accessTokenExpiration.name,
+      value: tokenResponse.accessTokenExpirationDateTime.toString());
+  authStorage.write(
+      key: SecureStoreKeys.refreshExpiresIn.name,
+      value: tokenResponse
+          .authorizationAdditionalParameters?['refresh_expires_in']);
+}
+
+Future<bool> checkCurrentAuthState() async {
+  validateCurrentAuthState(
+    accessToken: await authStorage.read(key: SecureStoreKeys.accesToken.name),
+    accessTokenExpiration:
+        await authStorage.read(key: SecureStoreKeys.accesToken.name),
+    refreshtoken: await authStorage.read(key: SecureStoreKeys.accesToken.name),
+    refreshExpiresIn:
+        await authStorage.read(key: SecureStoreKeys.accesToken.name),
+  );
+
+  return true;
+}
+
+Future<bool> validateCurrentAuthState({
+  required String? accessToken,
+  required String? accessTokenExpiration,
+  required String? refreshtoken,
+  required String? refreshExpiresIn,
+}) async {
+  return true;
+}
+
+bool isExpired(String? expiration) {
   return true;
 }
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gruene_app/constants/theme_data.dart';
@@ -16,110 +17,142 @@ import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:vector_graphics/vector_graphics.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool showLoadingIndicator = false;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            Flexible(
-                flex: 3,
-                child: SliderCarousel(
-                  showPrevNextButton: true,
-                  showIndicator: true,
-                  backgroundColor: Colors.transparent,
-                  activeDotColor: Theme.of(context).colorScheme.primary,
-                  deactiveDotColor: lightGrey,
-                  iconColor: Colors.white,
-                  rightIcon: Icons.arrow_forward,
-                  leftIcon: Icons.arrow_back,
-                  iconSize: 35,
-                  backgroundImage: SvgPicture(
-                      AssetBytesLoader(Assets.images.gruenenTopicOekologieSvg),
-                      height: size.height / 100 * 60),
-                  controlsBackground: Theme.of(context).primaryColor,
-                  pages: [
-                    SliderCarouselPage(
-                      backgroundColorText: Theme.of(context).primaryColor,
-                      backgroundColorImage: Colors.blue,
-                      preserveImageSpace: false,
-                      title: "Schau Dir Deine neue Mitglieder-App an",
-                      titleFontWeight: FontWeight.bold,
-                      titleFontSize: 33,
-                      body:
-                          "First impressions are everything in business, even in chat service. It’s important to show professionalism and courtesy from the start",
+            Column(
+              children: [
+                Flexible(
+                    flex: 3,
+                    child: SliderCarousel(
+                      showPrevNextButton: true,
+                      showIndicator: true,
+                      backgroundColor: Colors.transparent,
+                      activeDotColor: Theme.of(context).colorScheme.primary,
+                      deactiveDotColor: lightGrey,
+                      iconColor: Colors.white,
+                      rightIcon: Icons.arrow_forward,
+                      leftIcon: Icons.arrow_back,
+                      iconSize: 35,
+                      backgroundImage: SvgPicture(
+                          AssetBytesLoader(
+                              Assets.images.gruenenTopicOekologieSvg),
+                          height: size.height / 100 * 60),
+                      controlsBackground: Theme.of(context).primaryColor,
+                      pages: [
+                        SliderCarouselPage(
+                          backgroundColorText: Theme.of(context).primaryColor,
+                          backgroundColorImage: Colors.blue,
+                          preserveImageSpace: false,
+                          title: "Schau Dir Deine neue Mitglieder-App an",
+                          titleFontWeight: FontWeight.bold,
+                          titleFontSize: 33,
+                          body:
+                              "First impressions are everything in business, even in chat service. It’s important to show professionalism and courtesy from the start",
+                        ),
+                        SliderCarouselPage(
+                          backgroundColorText: Theme.of(context).primaryColor,
+                          preserveImageSpace: false,
+                          title: "Coffee With Friends",
+                          body:
+                              "When your morning starts with a cup of coffee and you are used to survive the day with the same, then all your Instagram stories and snapchat streaks would stay filled up with coffee pictures",
+                        ),
+                        SliderCarouselPage(
+                          backgroundColorText: Theme.of(context).primaryColor,
+                          preserveImageSpace: false,
+                          title: "Mobile Application",
+                          body:
+                              "Mobile content marketing has also been found to enhance quick online actions and make follow-ups easier.",
+                        ),
+                        SliderCarouselPage(
+                          backgroundColorText: Theme.of(context).primaryColor,
+                          preserveImageSpace: false,
+                          title: "Content Team",
+                          body: "No two content marketing teams look the same.",
+                        ),
+                      ],
+                    )),
+                Expanded(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IgnorePointer(
+                      ignoring: showLoadingIndicator,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            showLoadingIndicator = true;
+                          });
+                          final sucess = await startLogin();
+                          if (sucess) {
+                            router.go(startScreen);
+                          } else {
+                            setState(() {
+                              showLoadingIndicator = false;
+                            });
+                            MyApp.scaffoldMessengerKey.currentState
+                                ?.showSnackBar(const SnackBar(
+                                    content: Center(
+                              child: Text('Fehler beim Login '),
+                            )));
+                          }
+                        },
+                        child: Text(AppLocalizations.of(context)!.login,
+                            style: Theme.of(context)
+                                .primaryTextTheme
+                                .bodyLarge
+                                ?.copyWith(color: Colors.white)),
+                      ),
                     ),
-                    SliderCarouselPage(
-                      backgroundColorText: Theme.of(context).primaryColor,
-                      preserveImageSpace: false,
-                      title: "Coffee With Friends",
-                      body:
-                          "When your morning starts with a cup of coffee and you are used to survive the day with the same, then all your Instagram stories and snapchat streaks would stay filled up with coffee pictures",
-                    ),
-                    SliderCarouselPage(
-                      backgroundColorText: Theme.of(context).primaryColor,
-                      preserveImageSpace: false,
-                      title: "Mobile Application",
-                      body:
-                          "Mobile content marketing has also been found to enhance quick online actions and make follow-ups easier.",
-                    ),
-                    SliderCarouselPage(
-                      backgroundColorText: Theme.of(context).primaryColor,
-                      preserveImageSpace: false,
-                      title: "Content Team",
-                      body: "No two content marketing teams look the same.",
+                    const SizedBox(height: 20),
+                    Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                              AppLocalizations.of(context)!.registerMemberLabel,
+                              style:
+                                  Theme.of(context).primaryTextTheme.bodyLarge),
+                          const SizedBox(width: 20),
+                          Text(
+                            AppLocalizations.of(context)!.registerMember,
+                            style: Theme.of(context)
+                                .primaryTextTheme
+                                .bodyMedium
+                                ?.copyWith(
+                                    decoration: TextDecoration.underline),
+                          ),
+                        ]),
+                    Flexible(
+                      flex: 2,
+                      child: DatImpContainer(),
                     ),
                   ],
-                )),
-            Expanded(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  onPressed: () async {
-                    final sucess = await startLogin();
-                    if (sucess) {
-                      router.go(startScreen);
-                    } else {
-                      MyApp.scaffoldMessengerKey.currentState
-                          ?.showSnackBar(const SnackBar(
-                              content: Center(
-                        child: Text('Fehler beim Login '),
-                      )));
-                    }
-                  },
-                  child: Text(AppLocalizations.of(context)!.login,
-                      style: Theme.of(context)
-                          .primaryTextTheme
-                          .bodyLarge
-                          ?.copyWith(color: Colors.white)),
-                ),
-                const SizedBox(height: 20),
-                Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(AppLocalizations.of(context)!.registerMemberLabel,
-                          style: Theme.of(context).primaryTextTheme.bodyLarge),
-                      const SizedBox(width: 20),
-                      Text(
-                        AppLocalizations.of(context)!.registerMember,
-                        style: Theme.of(context)
-                            .primaryTextTheme
-                            .bodyMedium
-                            ?.copyWith(decoration: TextDecoration.underline),
-                      ),
-                    ]),
-                Flexible(
-                  flex: 2,
-                  child: DatImpContainer(),
-                ),
+                ))
               ],
-            ))
+            ),
+            if (showLoadingIndicator) ...[
+              const Positioned.fill(
+                  child: Align(
+                      alignment: Alignment.center,
+                      child: SpinKitThreeBounce(
+                        color: Colors.white,
+                        size: 50.0,
+                      )))
+            ],
           ],
         ),
       ),
