831f5be4d3ab24b73c6730ce4109783e09f6d7eb
Lars Boß <lars.boss@prodyna.com>
2023-03-28 10:51:19 +0200

    Fixed point 1-6 of ticket #72. Closing #72

--- lib/screens/intro/intro_screen.dart
+++ lib/screens/intro/intro_screen.dart
@@ -5,6 +5,7 @@ import 'package:gruene_app/gen/assets.gen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:gruene_app/routing/routes.dart';
 import 'package:gruene_app/widget/modal_top_line.dart';
+import 'package:gruene_app/widget/privacy_imprint.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:vector_graphics/vector_graphics.dart';
 
@@ -17,84 +18,96 @@ class IntroScreen extends StatelessWidget {
     final panelMinHeight = size.height / 100 * 10;
     return Scaffold(
         body: SafeArea(
-      child: Stack(children: [
-        Positioned.fill(
-          child: Align(
-            alignment: Alignment.center,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  flex: 5,
-                  child: SvgPicture(
-                      AssetBytesLoader(Assets.images.grueneTopicEconomySvg),
-                      height: size.height / 100 * 60),
-                ),
-                Flexible(
-                  flex: 3,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 20),
-                    child: Text(
-                      textAlign: TextAlign.center,
-                      AppLocalizations.of(context)!.introHeadline1,
-                      style: Theme.of(context).primaryTextTheme.displayLarge,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.center,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: SvgPicture(
+                        AssetBytesLoader(Assets.images.grueneTopicEconomySvg),
+                        height: size.height / 100 * 60),
+                  ),
+                  Flexible(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: Text(
+                        textAlign: TextAlign.left,
+                        AppLocalizations.of(context)!.introHeadline1,
+                        style: Theme.of(context).primaryTextTheme.displayLarge,
+                      ),
                     ),
                   ),
-                ),
-                Flexible(
-                  flex: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Text(
-                      textAlign: TextAlign.center,
-                      AppLocalizations.of(context)!.introHeadline2,
-                      style: Theme.of(context).primaryTextTheme.bodyMedium,
+                  Flexible(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        AppLocalizations.of(context)!.introHeadline2,
+                        style: Theme.of(context).primaryTextTheme.bodyMedium,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: EdgeInsets.only(bottom: panelMinHeight + 10),
-            child: ElevatedButton(
-                onPressed: () => context.go(login),
-                child: Text(AppLocalizations.of(context)!.login,
-                    style: const TextStyle(color: Colors.white))),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: panelMinHeight + 50),
+              child: ElevatedButton(
+                  onPressed: () => context.go(login),
+                  child: Text(AppLocalizations.of(context)!.login,
+                      style: const TextStyle(color: Colors.white))),
+            ),
           ),
-        ),
-        SlidingUpPanel(
-          minHeight: panelMinHeight,
-          maxHeight: size.height,
-          parallaxEnabled: true,
-          backdropEnabled: true,
-          backdropColor: Theme.of(context).colorScheme.secondary,
-          panel: Container(
-            color: Theme.of(context).colorScheme.secondary,
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 5,
-                ),
-                const ModalTopLine(color: Colors.white),
-                const SizedBox(
-                  height: 10,
-                ),
-                Center(
-                  child: Text(
-                    AppLocalizations.of(context)!.tourSliderIntroText,
-                    style: Theme.of(context).primaryTextTheme.titleMedium!
-                      ..copyWith(color: Colors.white),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: panelMinHeight + 10),
+              child: DatImpContainer(),
+            ),
+          ),
+          SlidingUpPanel(
+            minHeight: panelMinHeight,
+            maxHeight: size.height,
+            parallaxEnabled: true,
+            backdropEnabled: true,
+            backdropColor: Theme.of(context).colorScheme.secondary,
+            panel: Container(
+              color: Theme.of(context).colorScheme.secondary,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 5,
                   ),
-                ),
-              ],
+                  const ModalTopLine(color: Colors.white),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                      child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 80),
+                    child: Text(
+                      AppLocalizations.of(context)!.tourSliderIntroText,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).primaryTextTheme.titleMedium!
+                        ..copyWith(color: Colors.white),
+                    ),
+                  )),
+                ],
+              ),
             ),
           ),
-        )
-      ]),
+        ],
+      ),
     ));
   }
 }
