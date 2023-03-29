daf9bcfad9e813cac50aa84e8d18622c7d66eba6
DominikP30 <dominik.peters@prodyna.com>
2023-03-29 11:11:56 +0200

    add Test for token validation

--- lib/net/authentication/authentication.dart
+++ lib/net/authentication/authentication.dart
@@ -2,6 +2,8 @@ import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:gruene_app/common/logger.dart';
 
+enum AccessTokenState { authenticated, unauthenticated, refreshable, expired }
+
 enum SecureStoreKeys {
   accesToken,
   refreshtoken,
@@ -44,7 +46,14 @@ Future<bool> refreshToken(String? refreshToken) async {
     if (res == null) {
       return false;
     }
-    saveTokensInSecureStorage(res);
+    saveTokenValuesInSecureStorage(
+      accessToken: res.accessToken,
+      accessTokenExpiration: res.accessTokenExpirationDateTime.toString(),
+      refreshtoken: res.refreshToken,
+      // TODO: Check if value Exist
+      refreshExpiresIn: res.tokenAdditionalParameters?['refresh_expires_in'],
+    );
+    return true;
   } on Exception catch (e, st) {
     logger.d('Fail on Authentication', [e, st]);
     return false;
@@ -79,7 +88,13 @@ Future<bool> startLogin() async {
     if (result == null) {
       return false;
     }
-    saveTokensInSecureStorage(result);
+    saveTokenValuesInSecureStorage(
+      accessToken: result.accessToken,
+      accessTokenExpiration: result.accessTokenExpirationDateTime.toString(),
+      refreshtoken: result.refreshToken,
+      refreshExpiresIn:
+          result.authorizationAdditionalParameters?['refresh_expires_in'],
+    );
   } on Exception catch (e, st) {
     logger.d('Fail on Authentication', [e, st]);
     return false;
@@ -87,43 +102,66 @@ Future<bool> startLogin() async {
   return true;
 }
 
-void saveTokensInSecureStorage(AuthorizationTokenResponse tokenResponse) {
+void saveTokenValuesInSecureStorage({
+  required String? accessToken,
+  required String? refreshtoken,
+  required String? accessTokenExpiration,
+  required String? refreshExpiresIn,
+}) {
+  authStorage.write(key: SecureStoreKeys.accesToken.name, value: accessToken);
   authStorage.write(
-      key: SecureStoreKeys.accesToken.name, value: tokenResponse.accessToken);
-  authStorage.write(
-      key: SecureStoreKeys.refreshtoken.name,
-      value: tokenResponse.refreshToken);
+      key: SecureStoreKeys.refreshtoken.name, value: refreshtoken);
   authStorage.write(
       key: SecureStoreKeys.accessTokenExpiration.name,
-      value: tokenResponse.accessTokenExpirationDateTime.toString());
+      value: accessTokenExpiration);
   authStorage.write(
-      key: SecureStoreKeys.refreshExpiresIn.name,
-      value: tokenResponse
-          .authorizationAdditionalParameters?['refresh_expires_in']);
+      key: SecureStoreKeys.refreshExpiresIn.name, value: refreshExpiresIn);
 }
 
+// Check if the User has a valid Login
 Future<bool> checkCurrentAuthState() async {
-  validateCurrentAuthState(
+  final refresh = await authStorage.read(key: SecureStoreKeys.accesToken.name);
+  var res = validateAccessToken(
     accessToken: await authStorage.read(key: SecureStoreKeys.accesToken.name),
     accessTokenExpiration:
         await authStorage.read(key: SecureStoreKeys.accesToken.name),
-    refreshtoken: await authStorage.read(key: SecureStoreKeys.accesToken.name),
+    refreshToken: refresh,
     refreshExpiresIn:
         await authStorage.read(key: SecureStoreKeys.accesToken.name),
   );
-
-  return true;
+  switch (res) {
+    case AccessTokenState.authenticated:
+      return true;
+    case AccessTokenState.unauthenticated:
+      return false;
+    case AccessTokenState.refreshable:
+      return refreshToken(refresh);
+    case AccessTokenState.expired:
+      return false;
+  }
 }
 
-Future<bool> validateCurrentAuthState({
+AccessTokenState validateAccessToken({
   required String? accessToken,
   required String? accessTokenExpiration,
-  required String? refreshtoken,
+  required String? refreshToken,
   required String? refreshExpiresIn,
-}) async {
-  return true;
-}
-
-bool isExpired(String? expiration) {
-  return true;
+}) {
+  if (accessToken == null) {
+    return AccessTokenState.unauthenticated;
+  } else if (accessTokenExpiration == null) {
+    return AccessTokenState.unauthenticated;
+  } else if (DateTime.now().isAfter(DateTime.parse(accessTokenExpiration))) {
+    if (refreshExpiresIn == null) {
+      return AccessTokenState.unauthenticated;
+    }
+    if (refreshToken == null ||
+        DateTime.now().isAfter(DateTime.parse(refreshExpiresIn))) {
+      return AccessTokenState.expired;
+    } else {
+      return AccessTokenState.refreshable;
+    }
+  } else {
+    return AccessTokenState.authenticated;
+  }
 }
