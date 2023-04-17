f65cf65b0502913b459c89e36588f6bf8aba1374
DominikP30 <dominik.peters@prodyna.com>
2023-04-17 14:04:25 +0200

    adjust spaces card

--- lib/screens/more/screens/member_card/member_card.dart
+++ lib/screens/more/screens/member_card/member_card.dart
@@ -12,181 +12,174 @@ class MemberCardScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: LayoutBuilder(builder: (ctx, con) {
-        return Stack(
-          alignment: Alignment.center,
-          fit: StackFit.expand,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Container(
-                width: double.infinity,
-                height: double.infinity,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12.0),
-                  ),
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Color(0xff19B457),
-                      Color(0xff145F31),
-                    ],
-                    stops: [
-                      0.3076,
-                      1.1029,
-                    ],
-                    transform: GradientRotation(2.085),
-                  ),
-                ),
-                child: Stack(
-                  children: [
-                    const Positioned.fill(
-                      top: -100,
-                      left: -50,
-                      child: CircleAvatar(
-                        backgroundColor: Color(0xff19B457),
-                        radius: 120,
-                      ),
-                    ),
-                    const Positioned.fill(
-                      top: -20,
-                      left: -40,
-                      child: CircleAvatar(
-                        backgroundColor: Color.fromARGB(201, 25, 180, 87),
-                        radius: 60,
-                      ),
-                    ),
-                    Positioned.fill(
-                      top: 80,
-                      left: -70,
-                      child: CircleAvatar(
-                        backgroundColor: Theme.of(context).primaryColor,
-                        radius: 60,
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Mitgliedskarte',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyMedium
-                                      ?.copyWith(
-                                        color: Colors.white,
-                                      ),
-                                ),
-                                Text(
-                                  'Rosa',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .displaySmall
-                                      ?.copyWith(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  'Rosenberg',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .displaySmall
-                                      ?.copyWith(
-                                          height: 0.85,
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  'Kreisverband',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyMedium
-                                      ?.copyWith(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        const Spacer(),
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                RotatedBox(
-                                  quarterTurns:
-                                      3, // specify the number of quarter turns (90 degrees)
-                                  child: Row(
-                                    children: [
-                                      SvgPicture(AssetBytesLoader(
-                                          Assets.images.sunflowerWhiteSvg)),
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              "Mitgliedernummer:",
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .bodyMedium
-                                                  ?.copyWith(
-                                                      color: Colors.white),
-                                            ),
-                                            Text(
-                                              "0123456789",
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .bodyMedium
-                                                  ?.copyWith(
-                                                      color: Colors.white),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                const Spacer(),
-                                Container(
-                                  height: 200,
-                                  width: 200,
-                                  padding: const EdgeInsets.all(8.0),
-                                  decoration: const BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(12.0),
-                                    ),
-                                  ),
-                                  child: QrImage(
-                                    backgroundColor: Colors.white,
-                                    data: "0123456789",
-                                    version: QrVersions.auto,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+        return Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xff19B457),
+                    Color(0xff145F31),
                   ],
+                  stops: [
+                    0.3076,
+                    1.1029,
+                  ],
+                  transform: GradientRotation(2.085),
                 ),
               ),
-            )
-          ],
+              child: Stack(
+                fit: StackFit.passthrough,
+                children: [
+                  const Positioned(
+                    top: -40,
+                    left: -60,
+                    child: CircleAvatar(
+                      radius: 180,
+                      backgroundColor: Color(0xff19B457),
+                    ),
+                  ),
+                  const Positioned(
+                    top: 40,
+                    left: 60,
+                    child: CircleAvatar(
+                      radius: 280,
+                      backgroundColor: Color.fromARGB(161, 5, 129, 55),
+                    ),
+                  ),
+                  Positioned(
+                    top: -70,
+                    left: -30,
+                    child: CircleAvatar(
+                      radius: 150,
+                      backgroundColor: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Mitgliedskarte',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium
+                                    ?.copyWith(
+                                      color: Colors.white,
+                                    ),
+                              ),
+                              Text(
+                                'Rosa',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .displaySmall
+                                    ?.copyWith(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                'Rosenberg',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .displaySmall
+                                    ?.copyWith(
+                                        height: 0.85,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                'Kreisverband',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium
+                                    ?.copyWith(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              RotatedBox(
+                                quarterTurns:
+                                    3, // specify the number of quarter turns (90 degrees)
+                                child: Row(
+                                  children: [
+                                    SvgPicture(AssetBytesLoader(
+                                        Assets.images.sunflowerWhiteSvg)),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "Mitgliedernummer:",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyMedium
+                                                ?.copyWith(color: Colors.white),
+                                          ),
+                                          Text(
+                                            "0123456789",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyMedium
+                                                ?.copyWith(color: Colors.white),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const Spacer(),
+                              Container(
+                                height: 200,
+                                width: 200,
+                                padding: const EdgeInsets.all(8.0),
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(12.0),
+                                  ),
+                                ),
+                                child: QrImage(
+                                  backgroundColor: Colors.white,
+                                  data: "0123456789",
+                                  version: QrVersions.auto,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         );
       }),
     );
