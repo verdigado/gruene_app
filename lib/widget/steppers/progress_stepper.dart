0b86adc11bd91ac15e7c648e0abab71b6c4a7cee
Lars Boß <lars.boss@prodyna.com>
2023-04-19 15:23:13 +0200

    write test for progress stepper

--- lib/widget/steppers/progress_stepper.dart
+++ lib/widget/steppers/progress_stepper.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-/**
- * ProgressStepper:
- * A progressbar with a current and total number of steps
- */
+///
+/// ProgressStepper:
+/// A progressbar with a current and total number of steps
+///
 
 class ProgressStepper extends StatelessWidget {
   // current page index
@@ -15,12 +15,18 @@ class ProgressStepper extends StatelessWidget {
   // constructor
   const ProgressStepper({
     super.key,
-    this.currentPage = 0,
+    this.currentPage = 1,
     this.stepLength = 1,
   });
 
   @override
   Widget build(BuildContext context) {
+    if (currentPage > stepLength) {
+      throw Exception('Current page is greater than step length');
+    }
+    if (currentPage < 1) {
+      throw Exception('Current page is less than 1');
+    }
     return Column(
       children: [
         Align(
@@ -29,7 +35,7 @@ class ProgressStepper extends StatelessWidget {
             padding: const EdgeInsets.only(left: 18, right: 18),
             child: Text(
               textAlign: TextAlign.right,
-              '${currentPage + 1} / $stepLength',
+              '${currentPage} / $stepLength',
             ),
           ),
         ),
@@ -43,7 +49,7 @@ class ProgressStepper extends StatelessWidget {
               curve: Curves.easeIn,
               tween: Tween<double>(
                 begin: 0,
-                end: (currentPage + 1) / stepLength,
+                end: currentPage / stepLength,
               ),
               builder: (context, value, _) => LinearProgressIndicator(
                     backgroundColor: Theme.of(context)
