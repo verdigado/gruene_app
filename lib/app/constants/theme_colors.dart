26480c0f254f62011540a8b6ab3d650c7b2c794e
Volkram Weber <volkram@volkramweber.de>
2024-10-27 11:24:45 +0100

    249: Add styles and icons

---
+++ lib/app/constants/theme_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const Color middleGrey = Color(0xFF9CABAF);
+  static const Color stateHoverGreen = Color(0xFF005437);
+}
---
+++ lib/app/constants/theme_text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ThemeTextStyles {
+  static TextStyle smallText = GoogleFonts.ptSans(
+    textStyle: TextStyle(
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      height: 1.3,
+      letterSpacing: 0.01,
+    ),
+  );
+}
