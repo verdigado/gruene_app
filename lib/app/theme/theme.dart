63ee32192213e465f15bc0c92f7518b45031fcfb
Steffen Kleinle <steffen.kleinle@mailbox.org>
2024-11-08 14:03:52 +0100

    201: Make theme colors and texts consistent

--- lib/app/theme/theme.dart
+++ lib/app/theme/theme.dart
@@ -4,21 +4,33 @@ import 'package:google_fonts/google_fonts.dart';
 class ThemeColors {
   // Primary Green Dark/State Hover Green (#005437)
   static const Color primary = Color(0xFF005437);
+
   // Secondary Light Green (#008939)
   static const Color secondary = Color(0xFF008939);
-  // Middle Grey (#9CABAF)
-  static const Color disabled = Color(0xFF9CABAF);
+
+  // Secondary Light Green (#008939)
+  static const Color tertiary = Color(0xFF46962B);
+
   // White (#FFFFFF)
   static const Color background = Color(0xFFFFFFFF);
+
   // Softer Grey (#F9FAFB)
   static const Color backgroundSecondary = Color(0xFFF9FAFB);
+
   // Black (#000000)
   static const Color text = Color(0xFF000000);
+
+  // Dark Grey (#343433)
+  static const Color textAccent = Color(0xFF343433);
+
+  // Middle Grey (#9CABAF)
+  static const Color textDisabled = Color(0xFF9CABAF);
+
 }
 
 class ThemeTextStyles {
   // TOOD use GrueneType font
-  static TextStyle appBarText = GoogleFonts.ptSans(
+  static TextStyle displayMedium = GoogleFonts.ptSans(
     textStyle: TextStyle(
       fontSize: 18,
       fontWeight: FontWeight.w900,
@@ -27,31 +39,9 @@ class ThemeTextStyles {
       letterSpacing: 0.02,
     ),
   );
-  static TextStyle body = GoogleFonts.ptSans(
-    textStyle: TextStyle(
-      fontSize: 14,
-      fontWeight: FontWeight.w400,
-      height: 1.6,
-      letterSpacing: 0.02,
-    ),
-  );
-  static TextStyle smallText = GoogleFonts.ptSans(
-    textStyle: TextStyle(
-      fontSize: 10,
-      fontWeight: FontWeight.w400,
-      height: 1.3,
-      letterSpacing: 0.01,
-    ),
-  );
-  static TextStyle h4 = GoogleFonts.ptSans(
-    textStyle: TextStyle(
-      fontSize: 18,
-      fontWeight: FontWeight.w400,
-      height: 1,
-      letterSpacing: 0.02,
-    ),
-  );
-  static TextStyle h4Title = GoogleFonts.ptSans(
+  static TextStyle displayLarge = displayMedium.copyWith(fontSize: 34, letterSpacing: 0.03);
+
+  static TextStyle titleMedium = GoogleFonts.ptSans(
     textStyle: TextStyle(
       fontSize: 18,
       fontWeight: FontWeight.w700,
@@ -59,24 +49,52 @@ class ThemeTextStyles {
       letterSpacing: 0.02,
     ),
   );
+
+  static TextStyle bodyMedium = GoogleFonts.ptSans(
+    textStyle: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      height: 1.6,
+      letterSpacing: 0.02,
+    ),
+  );
+
+  static TextStyle bodyLarge = bodyMedium.copyWith(fontSize: 18, height: 1);
+
+  static TextStyle labelSmall = GoogleFonts.ptSans(
+    textStyle: TextStyle(
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      height: 1.3,
+      letterSpacing: 0.01,
+    ),
+  );
 }
 
-final ThemeData appTheme = ThemeData(
+final ThemeData appTheme = ThemeData.light().copyWith(
   primaryColor: ThemeColors.primary,
   disabledColor: ThemeColors.disabled,
+  colorScheme: ThemeData.light().colorScheme.copyWith(
+    primary: ThemeColors.primary,
+    secondary: ThemeColors.secondary,
+    tertiary: ThemeColors.tertiary,
+    surface: ThemeColors.background,
+    surfaceDim: ThemeColors.backgroundSecondary,
+  ),
   textTheme: TextTheme(
-    displayMedium: ThemeTextStyles.appBarText,
-    bodyLarge: ThemeTextStyles.h4,
-    bodyMedium: ThemeTextStyles.body,
-    titleMedium: ThemeTextStyles.h4Title,
-    labelSmall: ThemeTextStyles.smallText,
+    displayLarge: ThemeTextStyles.displayLarge,
+    displayMedium: ThemeTextStyles.displayMedium,
+    titleMedium: ThemeTextStyles.titleMedium,
+    bodyLarge: ThemeTextStyles.bodyLarge,
+    bodyMedium: ThemeTextStyles.bodyMedium,
+    labelSmall: ThemeTextStyles.labelSmall,
   ),
   bottomNavigationBarTheme: BottomNavigationBarThemeData(
     backgroundColor: ThemeColors.background,
     selectedItemColor: ThemeColors.primary,
-    unselectedItemColor: ThemeColors.disabled,
-    selectedLabelStyle: ThemeTextStyles.smallText,
-    unselectedLabelStyle: ThemeTextStyles.smallText,
+    unselectedItemColor: ThemeColors.textDisabled,
+    selectedLabelStyle: ThemeTextStyles.labelSmall,
+    unselectedLabelStyle: ThemeTextStyles.labelSmall,
   ),
   scaffoldBackgroundColor: ThemeColors.backgroundSecondary,
 );
