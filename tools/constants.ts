a5f6d161ee94aab6a216b46c487349ceded5b799
Steffen Kleinle <steffen.kleinle@mailbox.org>
2024-11-15 15:11:53 +0100

    187: Bump version

--- tools/constants.ts
+++ tools/constants.ts
@@ -1,7 +1,21 @@
 const VERSION_FILE = 'version.yaml'
 const MAIN_BRANCH = 'main'
 
+const PLATFORM_ANDROID = 'android'
+const PLATFORM_IOS = 'ios'
+
+const PLATFORMS = [PLATFORM_IOS, PLATFORM_ANDROID]
+
+type ReleaseInformation = {
+  platform: (typeof PLATFORMS)[number]
+  versionName: string
+}
+const tagId = ({ platform, versionName }: ReleaseInformation): string => `${versionName}-${platform}`
 export {
   VERSION_FILE,
   MAIN_BRANCH,
+  PLATFORM_ANDROID,
+  PLATFORM_IOS,
+  PLATFORMS,
+  tagId
 }
