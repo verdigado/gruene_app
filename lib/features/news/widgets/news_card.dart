69193a5598e303c35cd5ce9d2b1c5c320ac2b4bb
Steffen Kleinle <steffen.kleinle@mailbox.org>
2024-12-10 17:00:00 +0100

    211: Fix ripple effect and linear gradient

--- lib/features/news/widgets/news_card.dart
+++ lib/features/news/widgets/news_card.dart
@@ -4,6 +4,8 @@ import 'package:gruene_app/app/constants/routes.dart';
 import 'package:gruene_app/app/theme/theme.dart';
 import 'package:gruene_app/features/news/models/news_model.dart';
 
+const double imageHeight = 160;
+
 class NewsCard extends StatelessWidget {
   final void Function() setFavorite;
   final NewsModel news;
@@ -27,52 +29,72 @@ class NewsCard extends StatelessWidget {
       child: Card(
         color: theme.colorScheme.surface,
         margin: const EdgeInsets.symmetric(vertical: 8),
-        child: InkWell(
-          onTap: () => context.push('/news/${news.id}'),
-          customBorder: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                height: 160,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
-                  image: DecorationImage(
-                    image: AssetImage(news.image),
-                    fit: BoxFit.fitWidth,
+        child: Stack(
+          children: [
+            // Teaser image
+            Container(
+              height: imageHeight,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
+                image: DecorationImage(
+                  image: AssetImage(news.image),
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+            // Linear gradient on teaser image
+            Container(
+              height: imageHeight,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
+                gradient: LinearGradient(
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                  colors: [ThemeColors.text.withOpacity(0.5), Colors.transparent],
+                ),
+              ),
+            ),
+            // Card content
+            Positioned.fill(
+              top: imageHeight,
+              child: Container(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(news.title, style: theme.textTheme.titleLarge),
+                    SizedBox(height: 8),
+                    Expanded(
+                      child: Text(
+                        news.content,
+                      ),
+                    ),
+                    Chip(
+                      label: Text(news.creator),
+                      padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                      backgroundColor: theme.colorScheme.surface,
+                      shape: StadiumBorder(),
+                      side: BorderSide(
+                        color: theme.colorScheme.primary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // Pressable around the whole card
+            Positioned.fill(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () => context.push('/news/${news.id}'),
+                  customBorder: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
                 ),
               ),
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.all(16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(news.title, style: theme.textTheme.titleLarge),
-                      SizedBox(height: 8),
-                      Expanded(
-                        child: Text(
-                          news.content,
-                        ),
-                      ),
-                      Chip(
-                        label: Text(news.creator),
-                        padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                        backgroundColor: theme.colorScheme.surface,
-                        shape: StadiumBorder(),
-                        side: BorderSide(
-                          color: theme.colorScheme.primary,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
