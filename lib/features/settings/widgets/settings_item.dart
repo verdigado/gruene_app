3f523af139b3be76585574189f8fd7ccc09a6637
Steffen Kleinle <steffen.kleinle@mailbox.org>
2024-12-23 23:03:50 +0100

    429: Mark settings that are not implemented yet

--- lib/features/settings/widgets/settings_item.dart
+++ lib/features/settings/widgets/settings_item.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:gruene_app/app/theme/theme.dart';
 import 'package:gruene_app/app/widgets/icon.dart';
+import 'package:gruene_app/i18n/translations.g.dart';
 
 class SettingsItem extends StatelessWidget {
   final String title;
   final bool isExternal;
+  final bool isImplemented;
   final void Function() onPress;
 
-  const SettingsItem({super.key, required this.title, required this.onPress, this.isExternal = false});
+  const SettingsItem({
+    super.key,
+    required this.title,
+    required this.onPress,
+    this.isExternal = false,
+    this.isImplemented = true,
+  });
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return ListTile(
+      enabled: isImplemented,
       onTap: onPress,
-      title: Text(title, style: theme.textTheme.bodyLarge),
+      title: Text(
+        isImplemented ? title : '$title ${t.settings.notImplemented}',
+        style: theme.textTheme.bodyLarge?.apply(color: isImplemented ? ThemeColors.text : ThemeColors.textDisabled),
+      ),
       trailing: CustomIcon(
         path: isExternal ? 'assets/icons/external.svg' : 'assets/icons/chevron.svg',
         color: theme.disabledColor,
