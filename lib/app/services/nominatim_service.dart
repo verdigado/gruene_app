744d0331ee4aff9485374133842615a1d9b581c7
Christian Fiebrig <fiebrig@bitidee.de>
2024-12-23 17:19:24 +0100

    413: fix retrieving data for street and city from nominatim (#426)

--- lib/app/services/nominatim_service.dart
+++ lib/app/services/nominatim_service.dart
@@ -20,9 +20,22 @@ class AddressModel {
 
   const AddressModel({this.street = '', this.houseNumber = '', this.zipCode = '', this.city = ''});
 
+  /*
+  * More details on how to find and categorize "places" can be found in the OSM Wiki:
+  * https://wiki.openstreetmap.org/wiki/Template:Generic:Map_Features:place
+  * If road is absent we take hamlet oder dwelling as Street Information.
+  * For City we take in descending order city, town, or village, whatever is first.
+  * Hopefully, this will solve most issues with that kind of address composition.
+  */
   AddressModel.fromPlace(Place place)
-      : street = place.address?['road']?.toString() ?? '',
+      : street = place.address?['road']?.toString() ??
+            place.address?['hamlet']?.toString() ??
+            place.address?['isolated_dwelling']?.toString() ??
+            '',
         houseNumber = place.address?['house_number']?.toString() ?? '',
         zipCode = place.address?['postcode']?.toString() ?? '',
-        city = place.address?['city']?.toString() ?? '';
+        city = place.address?['city']?.toString() ??
+            place.address?['town']?.toString() ??
+            place.address?['village']?.toString() ??
+            '';
 }
