146f7a4977589d83ff7ea324aa52a90886136ca5
Christian Fiebrig <fiebrig@bitidee.de>
2024-12-27 18:40:15 +0100

    420: new popover design with address and close button
    - slider wheel now with more area to switch edit mode

--- lib/features/campaigns/screens/poster_detail.dart
+++ lib/features/campaigns/screens/poster_detail.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:gruene_app/app/theme/theme.dart';
 import 'package:gruene_app/features/campaigns/helper/campaign_constants.dart';
 import 'package:gruene_app/features/campaigns/models/posters/poster_detail_model.dart';
-import 'package:gruene_app/i18n/translations.g.dart';
 
-class PosterDetail extends StatefulWidget {
+class PosterDetail extends StatelessWidget {
   final PosterDetailModel poi;
 
   const PosterDetail({super.key, required this.poi});
 
   @override
-  State<StatefulWidget> createState() => PosterDetailState();
-}
-
-class PosterDetailState extends State<PosterDetail> {
-  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final addressTextStyle = theme.textTheme.labelSmall?.apply(color: ThemeColors.secondary, fontWeightDelta: 3);
 
     return Column(
       children: [
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 3),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: 100,
+                child: Text(
+                  poi.address.street,
+                  style: addressTextStyle,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Text(
+                poi.address.houseNumber,
+                style: addressTextStyle,
+              ),
+            ],
+          ),
+        ),
         Expanded(
           child: FutureBuilder(
-            future: Future.delayed(Duration.zero, () => widget.poi.thumbnailUrl),
+            future: Future.delayed(Duration.zero, () => poi.thumbnailUrl),
             builder: (context, snapshot) {
               if (!snapshot.hasData && !snapshot.hasError) {
                 return Image.asset(CampaignConstants.dummyImageAssetName);
@@ -33,10 +49,6 @@ class PosterDetailState extends State<PosterDetail> {
               );
             },
           ),
-        ),
-        Text(
-          t.campaigns.info_details,
-          style: theme.textTheme.labelSmall?.apply(color: Colors.black, fontWeightDelta: 2),
         ),
       ],
     );
