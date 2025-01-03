723d9bb392d257bb6b2d80865a755dadb3470d31
Christian Fiebrig <fiebrig@bitidee.de>
2025-01-03 12:25:19 +0100

    459: show info toast for focus area
    - when activating focus areas a toast pop ups
    - tapping shows info text for focus area

--- lib/features/campaigns/screens/screen_extensions.dart
+++ lib/features/campaigns/screens/screen_extensions.dart
@@ -154,3 +154,53 @@ mixin PosterValidator {
     return true;
   }
 }
+
+mixin FocusAreaInfo {
+  void showAboutFocusArea(BuildContext context) async {
+    final theme = Theme.of(context);
+    await showDialog<bool>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: ThemeColors.backgroundSecondary,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.info,
+                color: ThemeColors.textCancel,
+              ),
+              SizedBox(width: 6),
+              Text(
+                t.campaigns.infoToast.focusAreas_aboutTitle,
+                style: theme.textTheme.titleMedium?.apply(color: ThemeColors.textDark),
+              ),
+            ],
+          ),
+          content: Text(
+            t.campaigns.infoToast.focusAreas_aboutText,
+            textAlign: TextAlign.center,
+            style: theme.textTheme.labelMedium?.apply(
+              color: ThemeColors.textDark,
+              fontSizeDelta: 1,
+            ),
+          ),
+          actionsAlignment: MainAxisAlignment.end,
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.maybePop(context),
+              child: Text(
+                t.common.actions.close,
+                style: theme.textTheme.labelLarge?.apply(
+                  color: ThemeColors.textDark,
+                  fontWeightDelta: 2,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
