f81151e6dd3e5b5eeb27dbc9d1cafc9044ac85a7
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-01-10 11:54:18 +0100

    Move getFromApi to api_core

--- lib/app/services/gruene_api_core.dart
+++ lib/app/services/gruene_api_core.dart
@@ -2,9 +2,12 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:chopper/chopper.dart' as chopper;
+import 'package:chopper/chopper.dart';
 import 'package:flutter/foundation.dart';
+import 'package:get_it/get_it.dart';
 import 'package:gruene_app/app/auth/repository/auth_repository.dart';
 import 'package:gruene_app/app/constants/config.dart';
+import 'package:gruene_app/i18n/translations.g.dart';
 import 'package:gruene_app/swagger_generated_code/gruene_api.swagger.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
@@ -181,4 +184,20 @@ class AccessTokenAuthenticator implements chopper.Authenticator {
 
   @override
   chopper.AuthenticationCallback? get onAuthenticationSuccessful => null;
+}
+
+Future<T> getFromApi<S, T>({
+  required Future<Response<S>> Function(GrueneApi api) request,
+  required T Function(S data) map,
+}) async {
+  final GrueneApi api = GetIt.I<GrueneApi>();
+
+  final response = await request(api);
+  final body = response.body;
+
+  if (!response.isSuccessful || body == null) {
+    throw Exception('${t.error.apiError(statusCode: response.statusCode)}\n${response.base.request?.url}');
+  }
+
+  return map(body);
 }
--- lib/features/news/domain/news_api_service.dart
+++ lib/features/news/domain/news_api_service.dart
@@ -1,31 +1,12 @@
-import 'package:chopper/chopper.dart';
-import 'package:get_it/get_it.dart';
+import 'package:gruene_app/app/services/gruene_api_core.dart';
 import 'package:gruene_app/features/news/models/news_model.dart';
-import 'package:gruene_app/i18n/translations.g.dart';
-import 'package:gruene_app/swagger_generated_code/gruene_api.swagger.dart';
 
-Future<T> get<S, T>({
-  required Future<Response<S>> Function(GrueneApi api) request,
-  required T Function(S data) map,
-}) async {
-  final GrueneApi api = GetIt.I<GrueneApi>();
-
-  final response = await request(api);
-  final body = response.body;
-
-  if (!response.isSuccessful || body == null) {
-    throw Exception('${t.error.apiError(statusCode: response.statusCode)}\n${response.base.request?.url}');
-  }
-
-  return map(body);
-}
-
-Future<NewsModel> fetchNewsById(String newsId) async => get(
+Future<NewsModel> fetchNewsById(String newsId) async => getFromApi(
       request: (api) => api.v1NewsNewsIdGet(newsId: newsId),
       map: NewsModel.fromApi,
     );
 
-Future<List<NewsModel>> fetchNews() async => get(
+Future<List<NewsModel>> fetchNews() async => getFromApi(
       request: (api) => api.v1NewsGet(),
       map: (data) => data.data.map(NewsModel.fromApi).toList(),
     );
