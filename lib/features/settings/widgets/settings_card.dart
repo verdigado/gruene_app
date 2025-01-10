beb2d64ed2657da218ed711d2787704734346193
Niko Heller <acc.hellerniko@gmail.com>
2025-01-10 17:32:58 +0100

    520: add isEnabled option to settings card

--- lib/features/settings/widgets/settings_card.dart
+++ lib/features/settings/widgets/settings_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gruene_app/app/theme/theme.dart';
 import 'package:gruene_app/app/widgets/icon.dart';
 
 class SettingsCard extends StatelessWidget {
@@ -6,6 +7,7 @@ class SettingsCard extends StatelessWidget {
   final String title;
   final String subtitle;
   final bool isExternal;
+  final bool isEnabled;
   final void Function() onPress;
 
   const SettingsCard({
@@ -15,6 +17,7 @@ class SettingsCard extends StatelessWidget {
     required this.subtitle,
     required this.onPress,
     this.isExternal = false,
+    this.isEnabled = true,
   });
 
   @override
@@ -31,6 +34,7 @@ class SettingsCard extends StatelessWidget {
         ],
       ),
       child: Card(
+
         color: theme.colorScheme.surface,
         margin: const EdgeInsets.symmetric(vertical: 8),
         elevation: 0,
@@ -38,13 +42,13 @@ class SettingsCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
         ),
         child: ListTile(
-          onTap: onPress,
+          onTap: isEnabled ? onPress : null,
           contentPadding: EdgeInsets.symmetric(horizontal: 12),
-          title: Text(title, style: theme.textTheme.titleSmall),
-          subtitle: Text(subtitle),
+          title: Text(title, style: theme.textTheme.titleSmall?.apply(color: isEnabled ? ThemeColors.text : ThemeColors.textDisabled),),
+          subtitle: Text(subtitle, style: TextStyle(color: isEnabled ? ThemeColors.text : ThemeColors.textDisabled)),
           leading: ClipRRect(
             borderRadius: BorderRadius.circular(8.0),
-            child: Image.asset(icon, height: 48, width: 48),
+            child: Image.asset(icon, height: 48, width: 48, color: isEnabled ? null : Colors.grey, colorBlendMode: isEnabled ? null : BlendMode.saturation),
           ),
           trailing: Padding(
             padding: const EdgeInsets.only(right: 10),
