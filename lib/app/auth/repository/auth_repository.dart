2d437fa9a8a12f61899e82d35edcf0b921485acc
Volkram Weber <volkram@volkramweber.de>
2025-01-14 15:17:53 +0100

    511: poll auth challenges during login if configured and auto approve

--- lib/app/auth/repository/auth_repository.dart
+++ lib/app/auth/repository/auth_repository.dart
@@ -6,15 +6,22 @@ import 'package:get_it/get_it.dart';
 import 'package:gruene_app/app/constants/config.dart';
 import 'package:gruene_app/app/constants/secure_storage_keys.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:keycloak_authenticator/api.dart';
 import 'package:logger/logger.dart';
 
 class AuthRepository {
   final FlutterAppAuth _appAuth = FlutterAppAuth();
   final _secureStorage = GetIt.instance<FlutterSecureStorage>();
   final Logger _logger = Logger();
+  Timer? _pollingTimer;
+  final AuthenticatorService _authenticatorService = GetIt.I<AuthenticatorService>();
+  Authenticator? _authenticator;
 
   Future<bool> signIn() async {
     try {
+      _authenticator = await _authenticatorService.getFirst();
+      if (_authenticator != null) _pollForChallenge();
+
       final AuthorizationTokenResponse result = await _appAuth.authorizeAndExchangeCode(
         AuthorizationTokenRequest(
           Config.oidcClientId,
@@ -25,6 +32,8 @@ class AuthRepository {
         ),
       );
 
+      if (_authenticator != null) _stopPolling();
+
       await _secureStorage.write(key: SecureStorageKeys.accessToken, value: result.accessToken);
       await _secureStorage.write(key: SecureStorageKeys.idToken, value: result.idToken);
       await _secureStorage.write(key: SecureStorageKeys.refreshToken, value: result.refreshToken);
@@ -34,6 +43,7 @@ class AuthRepository {
     } catch (e) {
       _logger.w('Sign-in was not successful: $e');
     }
+    if (_authenticator != null) _stopPolling();
     return false;
   }
 
@@ -92,5 +102,37 @@ class AuthRepository {
     await _secureStorage.delete(key: SecureStorageKeys.accessToken);
     await _secureStorage.delete(key: SecureStorageKeys.idToken);
     await _secureStorage.delete(key: SecureStorageKeys.refreshToken);
+  }
+
+  Future<void> _pollForChallenge() async {
+    final startTime = DateTime.now();
+    final timeout = Duration(seconds: 120);
+
+    _pollingTimer = Timer.periodic(Duration(seconds: 1), (timer) async {
+      try {
+        if (DateTime.now().difference(startTime) > timeout) {
+          _stopPolling();
+          return;
+        }
+
+        final challenge = await _authenticator!.fetchChallenge();
+        if (challenge != null) {
+          _stopPolling();
+          await _authenticator!.reply(
+            challenge: challenge,
+            granted: true,
+          );
+        }
+      } catch (e) {
+        _logger.w('Error during challenge polling: $e');
+      }
+    });
+  }
+
+  void _stopPolling() {
+    if (_pollingTimer != null && _pollingTimer!.isActive) {
+      _pollingTimer!.cancel();
+      _pollingTimer = null;
+    }
   }
 }
