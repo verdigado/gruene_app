2873ba813aacd619239889a12a8149314c36554e
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-01-24 14:12:53 +0100

    212: Render tab views

--- lib/features/news/screens/news_screen.dart
+++ lib/features/news/screens/news_screen.dart
@@ -5,9 +5,7 @@ import 'package:gruene_app/app/widgets/app_bar.dart';
 import 'package:gruene_app/app/widgets/main_layout.dart';
 import 'package:gruene_app/app/widgets/rounded_icon_button.dart';
 import 'package:gruene_app/app/widgets/tab_bar.dart';
-import 'package:gruene_app/features/news/domain/news_api_service.dart';
-import 'package:gruene_app/features/news/models/news_model.dart';
-import 'package:gruene_app/features/news/widgets/news_card.dart';
+import 'package:gruene_app/features/news/widgets/news_list.dart';
 import 'package:gruene_app/i18n/translations.g.dart';
 
 class NewsScreen extends StatefulWidget {
@@ -53,18 +51,9 @@ class _NewsScreenState extends State<NewsScreen> with SingleTickerProviderStateM
           onTap: (index) => setState(() => _tabController.index = index),
         ),
       ),
-      child: FutureLoadingScreen(
-        load: fetchNews,
-        buildChild: (List<NewsModel>? data) {
-          if (data == null || data.isEmpty) {
-            return ErrorScreen(error: t.news.noResults, retry: fetchNews);
-          }
-          return ListView.builder(
-            padding: const EdgeInsets.all(16),
-            itemCount: data.length,
-            itemBuilder: (context, index) => NewsCard(news: data[index]),
-          );
-        },
+      child: TabBarView(
+        controller: _tabController,
+        children: widget.tabs.map((tab) => NewsList(bookmarked: tab == t.news.bookmarked)).toList(),
       ),
     );
   }
